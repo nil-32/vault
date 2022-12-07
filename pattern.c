4dfb287880f9297293661ed94fbc3dfda647c19f
nil <113453210+nil-32@users.noreply.github.com>
2022-12-07 02:40:59 -0800

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+int main()
+{
+    int x, column = 1, row = 1;
+
+    printf("kaca kadar? : ");
+    scanf("%d", &x);
+    printf("\n");
+
+    for(row = 1; row <= 2*x-1; row++){
+            for(column = 1; column <= 2*x-1 ; column++)
+                printf("%d ",MAX(abs(x-row),abs(x-column))+1);
+
+            printf("\n");
+    }
+
+    return 0;
+}
