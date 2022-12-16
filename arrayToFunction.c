550927d5127e03bd634a7d1103097bc914a284b8
nil <113453210+nil-32@users.noreply.github.com>
2022-12-16 06:11:54 -0800

    Create arrayToFunction.c

---
+++ arrayToFunction.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define X 100
+
+typedef struct matrix {
+    int t[100][100][100];
+} Matrix;
+
+Matrix getNumbers(int *kacblok,int *kacsatir , int *columnmaxi) {
+
+ int m, n , p ;
+    Matrix arr;
+
+    for( m = 0; m < *kacblok; m++)
+    {
+        for( n = 0; n < *kacsatir; n++)
+        {
+            for( p = 0; p < *columnmaxi; p++)
+            {
+            if (n==p){arr.t[m][n][p]=m+1;}
+            else{arr.t[m][n][p]=m;}
+            }
+        }
+    }
+
+    return arr;
+}
+void printNumbers(Matrix num1, int *kacblok,int *kacsatir,int *columnmaxi) {
+
+
+    int m, n , p ;
+    for( m = 0; m < *kacblok; m++)
+    {
+    printf("block %d\n",m+1);
+        for( n = 0; n < *kacsatir; n++)
+        {
+            for( p = 0; p < *columnmaxi; p++)
+            {
+
+            printf("%2d", num1.t[m][n][p]);
+				if(p==*columnmaxi-1)
+				{printf("\n");}
+            }
+        if(n==*kacsatir-1){printf("\n\n");}
+        }
+    printf("\n");
+    }
+}
+
+
+
+
+int main(){
+
+    int kacblok,kacsatir,columnmaxi;
+    printf("how many columns-rows-blocks would you like ?\n c-r-b\n");
+    scanf("%d %d %d",&kacblok,&kacsatir,&columnmaxi);
+    Matrix arr1;
+
+    arr1 = getNumbers(&kacblok,&kacsatir,&columnmaxi);
+    printNumbers(arr1,&kacblok,&kacsatir,&columnmaxi);
+}
